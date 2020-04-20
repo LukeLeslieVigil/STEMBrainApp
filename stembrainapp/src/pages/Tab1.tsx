ced35ac6a4743568f1210dc7be9577cbf9f3c046
James Hager <jimbodaman12@gmail.com>
2020-04-19 20:32:15 -0600

    Uploading my ionic server build
    uploading working ionic server build

--- stembrainapp/src/pages/Tab1.tsx
+++ stembrainapp/src/pages/Tab1.tsx
@@ -1,23 +1,36 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import React, {useState} from 'react';
+import { IonContent, IonHeader, IonPage, IonCard, IonCardHeader, IonCardContent, IonTitle, IonToolbar } from '@ionic/react';
+
 import './Tab1.css';
+import AddItem from '../AddItem';
+import ItemList from '../ItemList';
 
 const Tab1: React.FC = () => {
+
+  const [current, setCurrent] = useState(null);
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+      
       <IonContent>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+      {}
+      <IonCard>
+        
+        <IonCardHeader>
+        <h3>Group List of tasks:</h3>
+        </IonCardHeader>
+        <IonCardContent>
+          <AddItem initialValue={current} clear={()=>setCurrent(null)}/>
+
+          
+        </IonCardContent>
+        {}
+        <ItemList doEdit={setCurrent}/>
+      </IonCard>
       </IonContent>
     </IonPage>
   );
