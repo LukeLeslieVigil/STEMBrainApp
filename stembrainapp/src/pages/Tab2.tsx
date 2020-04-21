59960283aec1a7acd1f2ac24430b296406e6003d
LukeLeslieVigil <seventaco123@gmail.com>
2020-04-21 17:53:28 -0600

    Tab 2 - Main Page Design
    I designed the Tab 2 page up as it would look like if it were the main menu, keeping the tab 1 database stuff in tact but when we need to set the main page, the design is ready to go

--- stembrainapp/src/pages/Tab2.tsx
+++ stembrainapp/src/pages/Tab2.tsx
@@ -1,23 +1,49 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonButton, IonIcon } from '@ionic/react';
 import './Tab2.css';
+import { star, exit, flask } from 'ionicons/icons';
 
 const Tab2: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Tab 2</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <IonHeader>
+          <h1>Welcome to the STEM Brain App!</h1>
+        </IonHeader>
+        <IonCard>
+          <IonCardHeader>
+            <h1>Select an option:</h1>
+          </IonCardHeader>
+        </IonCard>
+        <IonCard>
+          <IonCardContent>
+            <IonButton expand="block" shape="round" size="large" href="tab1">
+              <IonIcon slot="start" icon={star} />
+              Start Learning
+            </IonButton>
+          </IonCardContent>
+        </IonCard>
+        <IonCard>
+          <IonCardContent>
+            <IonButton expand="block" shape="round" color="success" size="large" href="tab3">
+              <IonIcon slot="start" icon={flask} />
+              Learning Progress
+              </IonButton>
+          </IonCardContent>
+        </IonCard>
+        <IonCard>
+          <IonCardContent>
+            <IonButton expand="block" shape="round" color="danger" size="large" href="#">
+              <IonIcon slot="start" icon={exit} />
+              Exit
+            </IonButton>
+          </IonCardContent>
+        </IonCard>
       </IonContent>
     </IonPage>
   );
